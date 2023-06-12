4f1e93f0fe892c4e541dfed5ea4f728e28529ebe
sanghung4 <sanghung@dalml6116.attlocal.net>
2023-06-12 15:00:30 -0500

    update

---
+++ src/main/java/BaseTest.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseTest {
+
+    protected static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<>();
+    public CapabilitiesOptions capabilitiesOptions = new CapabilitiesOptions();
+
+    @BeforeMethod
+    @Parameters(value = {"browser"})
+    public void SetUp(String browser) throws MalformedURLException {
+        //Using local Docker container to run tests on Selenium Grid
+        driver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
+                capabilitiesOptions.getCapabilities(browser)));
+
+        //Using Kubernetes to run tests on Selenium Grid
+//        driver.set(new RemoteWebDriver(new URL("http://192.168.64.6:32518/wd/hub"),
+//                capabilitiesOptions.getCapabilities(browser)));
+        getDriver().manage().window().maximize();
+    }
+
+    public WebDriver getDriver() {
+        return driver.get();
+    }
+
+    @AfterMethod
+    public void TearDown() {
+        getDriver().quit();
+    }
+
+    @AfterClass void Terminate() {
+        driver.remove();
+    }
+}
+
